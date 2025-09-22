d81fd16cd319565f877e4d9b1f6720e6db324e63
Marcos <jorgede1097@gmail.com>
2025-09-22 18:54:28 -0300

    Second Commit...

---
+++ src/main/java/com/personal_blog/my_personal_blog/BlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal_blog.my_personal_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal_blog/my_personal_blog/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_blog.my_personal_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
